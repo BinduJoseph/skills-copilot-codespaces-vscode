f631d18ae0c9c5119ac0ca3f5beaf7a2b8b277c0
BinduJoseph <bindu.josin@gmail.com>
2023-12-08 14:45:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var memberDiv = document.getElementById("memberDiv");
+    var skillsDiv = document.getElementById("skillsDiv");
+    var projectsDiv = document.getElementById("projectsDiv");
+    var contactDiv = document.getElementById("contactDiv");
+    member.style.backgroundColor = "#1d1d1d";
+    skills.style.backgroundColor = "#1d1d1d";
+    projects.style.backgroundColor = "#1d1d1d";
+    contact.style.backgroundColor = "#1d1d1d";
+    member.style.color = "#fff";
+    skills.style.color = "#fff";
+    projects.style.color = "#fff";
+    contact.style.color = "#fff";
+    memberDiv.style.display = "block";
+    skillsDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "none";
+}   
